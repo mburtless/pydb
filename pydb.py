ecaf6d6b436ddb3781132b24171abbd23911dec4
Matthew Burtless <mburtless@threepnet.com>
2017-11-20 17:53:22 -0500

    Added some extra error handling

--- pydb.py
+++ pydb.py
@@ -8,7 +8,6 @@ class DBServer:
   :param server_address: Address to bind the server socket to
   :param server_port: Port to bind the server socket to
   """
-  #define socket options
   address_family = socket.AF_INET
   socket_type = socket.SOCK_STREAM
   request_queue_size = 1
@@ -16,53 +15,56 @@ class DBServer:
     #set instance vars
     self.server_address = server_address
     self.server_port = server_port
-    #create dict to store key value pairs for this DB
     self.key_value_db = {}
 
-    #Create socket
+    #Create socket, bind it and listen
     self.server_socket = socket.socket(self.address_family, self.socket_type)
-
-    #set socket options to allow resue
     self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    #bind socket
     self.server_socket.bind((server_address, server_port))
-    #listen on socket
-    self.server_socket.listen(self.request_queue_size)
 
     print 'Serving HTTP on port %s ...' % server_port
 
   def serve_requests(self):
-    server_socket = self.server_socket
-
-    #loop to listen for client connections and direct them to the handler
+    """Main loop to listen for client connections and route to request_handler"""
     while True:
+      #print('Listening for new requests')
+      self.server_socket.listen(self.request_queue_size)
       #create the client connection
-      client_connection, client_address = server_socket.accept()
+      client_connection, client_address = self.server_socket.accept()
       #call the request handler for the request and then close it and loop back and wait for another
       self.request_handler(client_connection)
 
   def request_handler(self, client_connection):
+    """Accepts incoming connections, routes them to the parser and triggers a response"""
     request = client_connection.recv(1024)
 
-    #print the request
-    #print self.request
+    #Make sure we recieved some data before proceeding
+    if not request:
+        response = 'Error: detected empty request'
+    else:
+        response = self.parse_request(request)
 
-    response = self.parse_request(request)
-
+    print response
     self.send_response(client_connection, response)
 
   def parse_request(self, request):
-    response = 'Hello World'
-    #we only care about the first line of the request
-    request_line = request.splitlines()[0]
+    """Method to parse requests by path"""
+    response=''
+    try:
+        request_line = request.splitlines()[0]
+        request_method, path, request_version = request_line.split()
+    except IndexError:
+        response = 'Error: Empty request'
+        return response
 
-    #split the request into it's components
-    request_method, path, request_version = request_line.split()
-
-    #split path into it's components
-    request_op, request_keyvalue = path.split('?')
-    request_op = request_op[1:]
+    try:
+        #split path into it's components, the operation requested and the keyvalue
+        request_op, request_keyvalue = path.split('?')
+        request_op = request_op[1:]
+    except ValueError:
+        response = """Error: Incorrect path (%s)
+        Requested URL must take the form http://%s:%s/[operation]?[value]""" % (path, self.server_address, self.server_port)
+        return response
 
     #handle request appropriately depending on operation requested
     if request_op == 'get':
@@ -72,35 +74,38 @@ class DBServer:
       request_key, request_value = request_keyvalue.split('=')
       response = self.set_value(request_key, request_value)
     else:
-      print('Error: unknown operation in URL. Must be either GET or SET.')
+      #print('Error: unknown operation in URL. Must be either GET or SET.')
+      response = 'Error: unknown operation in URL. Must be either GET or SET.'
 
-    #print('Request path is %s so op is %s and key is %s' % (path, request_op, request_key))
     return response
 
   def get_value(self, request_key):
+    """Getter to retrieve value for a passed key from the DB"""
     if request_key in self.key_value_db:
       result = self.key_value_db[request_key]
-      print('Value for key %s is %s' % (request_key, self.key_value_db[request_key]))
+      #print('Value for key %s is %s' % (request_key, self.key_value_db[request_key]))
     else:
       result = 'Error: Key ' + request_key + ' has not been set'
-      print('Key %s has not been set' % request_key)
+      #print('Key %s has not been set' % request_key)
 
     return result
 
   def set_value(self, request_key, request_value):
+    """Setter to set the value for a passed key"""
     self.key_value_db[request_key] = request_value
-    print('Setting value of %s to %s' % (request_key, request_value))
+    #print('Setting value of %s to %s' % (request_key, request_value))
     response = 'Key ' + request_key + ' has been set to ' + request_value
     return response
 
   def send_response(self, client_connection, response):
+    """Method to send a response to the client and close the connection"""
     http_response = "HTTP/1.1 200 OK\n\n" + response
     client_connection.sendall(http_response)
     client_connection.close()
 
 
-#def funtion to make the server, takes address and port to listen on as param
 def make_dbserver(server_address, server_port):
+  """Make an instance of DBServer on the passed IP and port"""
   server = DBServer(server_address, server_port)
   return server
 
